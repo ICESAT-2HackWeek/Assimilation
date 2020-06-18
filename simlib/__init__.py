506ced960ed270fe013154d4bf65d82ce58a80af
friedrichknuth <knuth@uw.edu>
2020-06-18 17:23:45 +0000

    update simlib

--- simlib/__init__.py
+++ simlib/__init__.py
@@ -2,10 +2,12 @@ from simlib.coregistration import reference_dem
 import simlib.atl06lib
 import simlib.icesatapi
 import simlib.config
+import simlib.utils
 
 __all__ = [
     'reference_dem',
     'atl06lib',
     'icesatapi',
     'config',
+    'utils',
 ]

830c6c8df4b34e8957376a304355f4746f84f74a
Whyjay Zheng <wz278@cornell.edu>
2020-06-18 00:08:33 +0000

    Replace bad quotation marks

--- simlib/atl06lib.py
+++ simlib/atl06lib.py
@@ -186,7 +186,7 @@ def read_atl06(fname, epsg, outdir='data', bbox=None):
 
 def read_h5(fname, vnames=None):
     """Read hdf5 file and return all variables"""
-    with h5py.File(fname, ‘r’) as f:
+    with h5py.File(fname, 'r') as f:
         if not vnames:
             vnames = [key for key in f.keys()]
         return np.column_stack([f[v][()] for v in vnames]), vnames
